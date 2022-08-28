5eb55b7bcdabcee924c65002bd6e55d8cd80d1c5
mushi333 <mujtaba.zahidi24@gmail.com>
2022-08-28 14:39:50 +1000

    login button links to home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,151 +1,11 @@
 import React from 'react';
 import {Routes, Route, useNavigate} from 'react-router-dom';
 import Background from './img/login_background.jpg';
-
-
-const Field = React.forwardRef(({label, type}, ref) => {
-  const labelStyle = {
-    margin: '10px 0 5px 0',
-    fontFamily: 'Arial, Helvetica, sans-serif',
-    fontSize: '15px',
-  };
-  
-  const inputStyle = {
-    margin: '5px 0 10px 0',
-    padding: '5px', 
-    border: '1px solid #bfbfbf',
-    borderRadius: '3px',
-    boxSizing: 'border-box',
-    width: '100%'
-  };
-
-  return (
-    <div>
-      <label style={labelStyle} >{label}</label>
-      <input ref={ref} type={type} style={inputStyle} />
-    </div>
-  );
-});
-
-const Form = ({onSubmit}) => {
-  const formStyle = {
-    margin: 'auto',
-    padding: '10px',
-    border: '1px solid #c9c9c9',
-    borderRadius: '5px',
-    background: '#f5f5f5',
-    width: '220px',
-    display: 'block'
-  };
-  
-  const submitStyle = {
-    margin: '10px 0 0 0',
-    padding: '7px 10px',
-    border: '1px solid #efffff',
-    borderRadius: '3px',
-    background: '#3085d6',
-    width: '100%', 
-    fontSize: '15px',
-    color: 'white',
-    display: 'block'
-  };
-
-  const usernameRef = React.useRef();
-  const passwordRef = React.useRef();
-  const handleSubmit = e => {
-      e.preventDefault();
-      const data = {
-          username: usernameRef.current.value,
-          password: passwordRef.current.value
-      };
-      onSubmit(data);
-  };
-  return (
-    <form style={formStyle} onSubmit={handleSubmit} >
-      <Field ref={usernameRef} label="Username:" type="text" />
-      <Field ref={passwordRef} label="Password:" type="password" />
-      <div>
-        <nav>
-        <button style={submitStyle} type="submit">Login</button>
-        </nav>
-      </div>
-    </form>
-  );
-};
-
-const Header = () => {
-  const headerStyle = {
-    padding: '30px',
-    margin: '0px',
-    // borderRadius: '25px',
-    textAlign: 'center',
-    background: '#5A5A5A',
-  };
-
-  const headerTextStyle = {
-    color: 'white',
-    fontSize: '20px',
-    fontFamily: 'Arial, Helvetica, sans-serif',
-  }
-
-  return (
-    <div style={headerStyle}>
-      <div style={headerTextStyle}>GymBro</div>
-    </div>
-  );
-}
-
-const Home = () => {
-  const homeStyle = {
-    display: 'flex',
-    flexDirection: 'row',
-    fontSize: '30px',
-    textAlign: 'center'
-  }
-
-  return (
-    <div style={homeStyle}>
-      <Message />
-      <Card />
-    </div>
-  );
-}
-
-const Message = () => {
-  const messageStyle = {
-    background: '#000000',
-    padding: '10px',
-    flex: '1',
-    color: 'white',
-    borderRadius: '25px',
-    border:' 2px solid #73AD21',
-    width: '200px',
-    margin: '20px',
-    top: '10px',
-    bottom: '10px'
-  }
-
-  return (
-    <div style={messageStyle}>
-      <h1>Hello World</h1>
-    </div>
-  );
-}
-
-const Card = () => {
-  const cardStyle = {
-    backgroundColor: 'FFFFFF',
-    padding: '10px',
-    flex: '2',
-    color: 'white'
-  }
-
-  return (
-    <div style={cardStyle}>
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Login from './components/Login';
 
 const App = () => {
   const backgroundStyle = {
@@ -154,11 +14,10 @@ const App = () => {
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
   };
-  
-  const appStyle = {
-    height: '250px',
-    display: 'flex'
-  };
+
+  const textStyle = {
+    color: 'white'
+  }
 
   const navigate = useNavigate();
 
@@ -168,20 +27,32 @@ const App = () => {
 
     return (
       <div style={backgroundStyle}>
+        <a href='/Home' onClick={navigateToHome} style={{textDecoration: "false"}}>
+        <Navbar bg="light">
+          <Container>
+            <Navbar.Brand>GymBro</Navbar.Brand>
+          </Container>
+        </Navbar>
+        </a>
         <Routes>
           <Route path="/" element={
-            <div>
-            <Header />
-            <div style={appStyle}>
-              <Form onSubmit={navigateToHome} />
-            </div>
-            </div>
+            <Container>
+              <Row>
+              <Col></Col>
+              <Col className="w-25 p-2 align-items-center">
+                <Login />
+              </Col>
+              <Col></Col>   
+              </Row>
+            </Container>
           } />
           <Route path="/Home" element={
-            <div>
-            <Header />
-            <Home />
-            </div>
+            <Container style={textStyle}>
+              <Row>
+                <Col>1 of 2</Col>
+                <Col xs={5}>1 of 2 (wider)</Col>
+              </Row>
+          </Container>
           } />
         </Routes>
       </div>
